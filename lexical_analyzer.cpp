037327714af242847b26cc8d29f144cd987ad93a
Matvey-Makaro <matvey.makaro@gmail.com>
2023-05-09 14:15:51 +0300

    Started making a lexical analyzer.

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -3,12 +3,76 @@
 //
 
 #include "lexical_analyzer.h"
+using namespace std;
 
-LexicalAnalyzer::LexicalAnalyzer(std::vector<NameTableItem> &name_table,
-                                 std::unordered_map<std::string_view, uint32_t> &name_to_id,
-                                 std::unordered_map<uint64_t, uint8_t> &row_to_command_size) :
-                                 _name_table(name_table), _name_to_id(name_to_id), _row_to_command_size(row_to_command_size)
+void LexicalAnalyzer::analyze()
 {
+    readch();
+    while(!_is_eof)
+    {
+        if(_state == States::START)
+            start_state();
+        else if(_state == States::ID_OR_KEY_WORD)
+            id_or_key_word_state();
+        else if(_state == States::NUM)
+            num_state();
+        else if(_state == States::DELIMITER)
+            delimiter_state();
+        else if(_state == States::STRING)
+            string_state();
+        else if(_state == States::COMMENT)
+            comment_state();
+    }
+}
+
+
+
+void LexicalAnalyzer::readch()
+{
+    _line_index++;
+    if(_line_index < _line.size())
+    {
+        getline(_fin, _line);
+        if(_fin.eof())
+            _is_eof = true;
+        _line_index = 0;
+    }
+
+    _ch = _line[_line_index];
+}
+
+void LexicalAnalyzer::start_state()
+{
+    while(isspace(_ch))
+        readch();
+
+    if(isalpha(_ch) || _ch == '_')
+        _state = States::ID_OR_KEY_WORD;
+    else if(isdigit(_ch))
+        _state = States::NUM;
+    else if(is_delimiter(_ch))
+        _state = States::DELIMITER;
+    else if(_ch == '\"')
+        _state = States::STRING;
+    else if(_ch == ';')
+        _state = States::COMMENT;
+    else if(_is_eof)
+        _state = States::END;
 
 }
 
+void LexicalAnalyzer::id_or_key_word_state()
+{
+    clear_buff();
+    while(isalnum(_ch) || _ch == '_')
+    {
+        _buff += _ch;
+        readch();
+    }
+
+}
+
+void LexicalAnalyzer::clear_buff()
+{
+    _buff = "";
+}
--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -4,19 +4,28 @@
 
 #pragma once
 
-#include "name_table_item.h"
+#include "common.h"
 
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <string_view>
+#include <fstream>
 
+enum class States
+{
+    START,
+    ID_OR_KEY_WORD,
+    NUM,
+    STRING,
+    DELIMITER,
+    COMMENT,
+    ERR,
+    END
+};
 
 class LexicalAnalyzer
 {
 public:
-    LexicalAnalyzer(std::vector<NameTableItem>& name_table, std::unordered_map<std::string_view, uint32_t>& name_to_id,
-                    std::unordered_map<uint64_t, uint8_t>& row_to_command_size);
+    LexicalAnalyzer(std::istream& in, LexemeTable& lexeme_table, NameTable& name_table, NameToID& name_to_id,
+                    RowToCommandSize& row_to_command_size, LiteralTable literal_table,
+                    IntegerToID& integer_to_id, StrToID& str_to_id);
     ~LexicalAnalyzer() = default;
 
     LexicalAnalyzer(const LexicalAnalyzer& other) = delete;
@@ -24,10 +33,37 @@ public:
     LexicalAnalyzer(LexicalAnalyzer&& other) = delete;
     LexicalAnalyzer& operator=(LexicalAnalyzer&& other) = delete;
 
-private:
-    std::vector<NameTableItem> _name_table;
-    std::unordered_map<std::string_view, uint32_t> _name_to_id;
-    std::unordered_map<uint64_t, uint8_t> _row_to_command_size;
+    void analyze();
 
+private:
+    void readch();
+    void start_state();
+    void id_or_key_word_state();
+    void num_state();
+    void string_state();
+    void delimiter_state();
+    void comment_state();
+
+    void clear_buff();
+
+private:
+    LexemeTable _lexeme_table;
+    NameTable _name_table;
+    NameToID _name_to_id;
+    RowToCommandSize _row_to_command_size;
+    LiteralTable _literal_table;
+    IntegerToID _integer_to_id;
+    StrToID _str_to_id;
+
+    std::ifstream& _fin;
+    char _ch = ' ';
+    uint64_t  _line_num = 0;
+    uint64_t  _col_num = 0;
+    std::string _buff;
+    std::string _line;
+    int _line_index = 0;
+    bool _is_eof = false;
+
+    States _state = States::START;
 };
 
