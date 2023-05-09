9700a071bd89ec8f39719402ba7bd9a7e27bab59
Matvey-Makaro <matvey.makaro@gmail.com>
2023-05-09 19:34:05 +0300

    Add id_or_key_word and num states.

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -3,12 +3,24 @@
 //
 
 #include "lexical_analyzer.h"
+
+#include <utility>
+#include <stdexcept>
 using namespace std;
+
+
+LexicalAnalyzer::LexicalAnalyzer(FILE* file, LexemeTable &lexeme_table, NameTable &name_table, NameToID &name_to_id,
+                                 LiteralTable &literal_table, IntegerToID &integer_to_id, StrToID &str_to_id)
+        : _file(file), _lexeme_table(lexeme_table), _name_table(name_table), _name_to_id(name_to_id),
+          _literal_table(literal_table), _integer_to_id(integer_to_id), _str_to_id(str_to_id)
+{
+    _lexeme_table.emplace_back();
+}
 
 void LexicalAnalyzer::analyze()
 {
     readch();
-    while(!_is_eof)
+    while(true)
     {
         if(_state == States::START)
             start_state();
@@ -22,23 +34,13 @@ void LexicalAnalyzer::analyze()
             string_state();
         else if(_state == States::COMMENT)
             comment_state();
+        else if(_state == States::END)
+        {
+            if(_lexeme_table.back().empty())
+                _lexeme_table.pop_back();
+            return;
+        }
     }
-}
-
-
-
-void LexicalAnalyzer::readch()
-{
-    _line_index++;
-    if(_line_index < _line.size())
-    {
-        getline(_fin, _line);
-        if(_fin.eof())
-            _is_eof = true;
-        _line_index = 0;
-    }
-
-    _ch = _line[_line_index];
 }
 
 void LexicalAnalyzer::start_state()
@@ -48,7 +50,7 @@ void LexicalAnalyzer::start_state()
 
     if(isalpha(_ch) || _ch == '_')
         _state = States::ID_OR_KEY_WORD;
-    else if(isdigit(_ch))
+    else if(isdigit(_ch) || _ch == '+' || _ch == '-')
         _state = States::NUM;
     else if(is_delimiter(_ch))
         _state = States::DELIMITER;
@@ -56,9 +58,8 @@ void LexicalAnalyzer::start_state()
         _state = States::STRING;
     else if(_ch == ';')
         _state = States::COMMENT;
-    else if(_is_eof)
+    else if(is_eof(_ch))
         _state = States::END;
-
 }
 
 void LexicalAnalyzer::id_or_key_word_state()
@@ -70,9 +71,121 @@ void LexicalAnalyzer::id_or_key_word_state()
         readch();
     }
 
+    auto key_words_it = key_words.find(_buff);
+    if(key_words_it != end(key_words))
+    {
+        auto lexeme_value = key_words_it->second;
+        add_lexeme(lexeme_value);
+    } else if(_name_to_id.count(_buff) == 0)
+    {
+        add_identifier_name(move(_buff));
+        auto id = _name_table.size() - 1;
+        add_lexeme(LexemeType::IDENTIFIER, id);
+    }
+    else add_lexeme(LexemeType::IDENTIFIER, _name_to_id[_buff]);
+
+    _state = States::START;
+}
+
+void LexicalAnalyzer::num_state()
+{
+    clear_buff();
+    if(_ch == '+' || _ch == '-')
+    {
+        _buff += _ch;
+        readch();
+    }
+    while(isdigit(_ch))
+    {
+        _buff += _ch;
+        readch();
+    }
+
+    if(!isspace(_ch) && _ch != ',')
+    {
+        _error_description = "Wrong characters after a number";
+        _state = States::ERR;
+    }
+
+    int64_t num;
+    try
+    {
+        num = stoll(_buff);
+    }
+    catch(std::invalid_argument&)
+    {
+        _error_description = "Wrong number";
+        _state = States::ERR;
+    }
+    catch(std::out_of_range&)
+    {
+        _error_description = "Number out or range";
+        _state = States::ERR;
+    }
+
+    auto _integer_to_id_it = _integer_to_id.find(num);
+    if(_integer_to_id_it == end(_integer_to_id))
+    {
+        add_integer_to_literal_table(num);
+        auto id = _integer_to_id.size() - 1;
+        add_lexeme(LexemeType::LITERAL, id);
+    }
+    else add_lexeme(LexemeType::LITERAL, _integer_to_id_it->second);
+
+    _state = States::START;
+}
+
+void LexicalAnalyzer::readch()
+{
+    if(_ch == '\n')
+    {
+        _line_num += 1;
+        _col_num = 0;
+        _lexeme_table.emplace_back();
+    }
+    else _col_num++;
+    _ch = fgetc(_file);
 }
 
 void LexicalAnalyzer::clear_buff()
 {
     _buff = "";
+}
+
+void LexicalAnalyzer::add_lexeme(LexemeValue value, int64_t id, size_t size)
+{
+    auto& curr_line = _lexeme_table.back();
+    curr_line.emplace_back(get_lexeme_type(value), value, id, size, _line_num, _col_num);
+}
+
+void LexicalAnalyzer::add_lexeme(LexemeType type, int64_t id, size_t size)
+{
+    auto& curr_line = _lexeme_table.back();
+    curr_line.emplace_back(type, LexemeValue::UNDEFINED, id, size, _line_num, _col_num);
+}
+
+void LexicalAnalyzer::add_identifier_name(std::string name)
+{
+    _name_table.emplace_back(move(name));
+    auto index = _name_table.size() - 1;
+    _name_to_id[_name_table[index].name] = index;
+}
+
+void LexicalAnalyzer::add_integer_to_literal_table(int64_t num)
+{
+    _literal_table.emplace_back(num);
+    auto id = _literal_table.size() - 1;
+    _integer_to_id[num] = id;
+}
+
+void LexicalAnalyzer::string_state() {
+
+}
+
+void LexicalAnalyzer::delimiter_state() {
+
+}
+
+void LexicalAnalyzer::comment_state() {
+
 }
--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -6,7 +6,7 @@
 
 #include "common.h"
 
-#include <fstream>
+#include <stdio.h>
 
 enum class States
 {
@@ -23,9 +23,8 @@ enum class States
 class LexicalAnalyzer
 {
 public:
-    LexicalAnalyzer(std::istream& in, LexemeTable& lexeme_table, NameTable& name_table, NameToID& name_to_id,
-                    RowToCommandSize& row_to_command_size, LiteralTable literal_table,
-                    IntegerToID& integer_to_id, StrToID& str_to_id);
+    LexicalAnalyzer(FILE* file, LexemeTable& lexeme_table, NameTable& name_table, NameToID& name_to_id,
+                    LiteralTable& literal_table, IntegerToID& integer_to_id, StrToID& str_to_id);
     ~LexicalAnalyzer() = default;
 
     LexicalAnalyzer(const LexicalAnalyzer& other) = delete;
@@ -36,7 +35,6 @@ public:
     void analyze();
 
 private:
-    void readch();
     void start_state();
     void id_or_key_word_state();
     void num_state();
@@ -44,26 +42,30 @@ private:
     void delimiter_state();
     void comment_state();
 
+    void readch();
     void clear_buff();
+    void add_lexeme(LexemeValue value, int64_t id = -1, size_t size = 0);
+    void add_lexeme(LexemeType type, int64_t id = -1, size_t size = 0);
+    void add_identifier_name(std::string name);
+    void add_integer_to_literal_table(int64_t num);
 
 private:
-    LexemeTable _lexeme_table;
-    NameTable _name_table;
-    NameToID _name_to_id;
-    RowToCommandSize _row_to_command_size;
-    LiteralTable _literal_table;
-    IntegerToID _integer_to_id;
-    StrToID _str_to_id;
+    LexemeTable& _lexeme_table;
+    NameTable& _name_table;
+    NameToID& _name_to_id;
+    LiteralTable& _literal_table;
+    IntegerToID& _integer_to_id;
+    StrToID& _str_to_id;
 
-    std::ifstream& _fin;
+    FILE* _file;
     char _ch = ' ';
     uint64_t  _line_num = 0;
     uint64_t  _col_num = 0;
     std::string _buff;
     std::string _line;
-    int _line_index = 0;
-    bool _is_eof = false;
 
     States _state = States::START;
+
+    std::string _error_description;
 };
 
